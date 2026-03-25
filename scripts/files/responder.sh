38d5f4d5daf59da9bc9943d608a9a99387fd59cc
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-03-25 11:59:50 +0000

    fix dockerfile syntax error

--- scripts/files/responder.sh
+++ scripts/files/responder.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PROJECT_DIR="/opt/diaries"
-SUBPROJECT_DIR="${PROJECT_DIR}/diaries-responder"
-COMMON_SUBPROJECT_DIR="${PROJECT_DIR}/diaries-common"
-
+APP_JAR="${APP_JAR:-/opt/diaries/lib/diaries-responder.jar}"
 CONFIG_FILE="${CONFIG_FILE:-/config/responder.json}"
 
-CLASSPATH="${SUBPROJECT_DIR}/bin/main:${SUBPROJECT_DIR}/src/main/resources/META-INF:${COMMON_SUBPROJECT_DIR}/bin/main:${SUBPROJECT_DIR}/runtime/*"
+exec java ${JAVA_OPTS:-} -jar "${APP_JAR}" --config "${CONFIG_FILE}"
 
-exec java ${JAVA_OPTS:-} \
-  -classpath "${CLASSPATH}" \
-  com.rsmaxwell.diaries.responder.Responder \
-  --config "${CONFIG_FILE}"
-  
