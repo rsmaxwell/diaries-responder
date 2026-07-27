641a97d5e0a5e72d2288e75f638e5a3a3fc064bf
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-07-27 12:41:20 +0100

    generate build-info in gradle

--- scripts/windows/run-responder.bat
+++ scripts/windows/run-responder.bat
@@ -4,14 +4,32 @@ setlocal
 set "SCRIPT_DIR=%~dp0"
 
 pushd "%SCRIPT_DIR%\..\..\.."
-set PROJECT_DIR=%CD%
-set "RESPONDER_DIR=%PROJECT_DIR%\diaries-responder"
+if errorlevel 1 (
+    echo ERROR: Unable to change to the Diaries project directory.
+    endlocal & exit /b 1
+)
 
-set "CLASSPATH=%RESPONDER_DIR%\bin\main;%RESPONDER_DIR%\runtime\*"
+set "PROJECT_DIR=%CD%"
+set "RESPONDER_DIR=%PROJECT_DIR%\diaries-responder"
+set "GENERATED_BUILD_INFO_DIR=%RESPONDER_DIR%\build\generated\resources\buildInfo"
+
 set "CONFIG_FILE=%USERPROFILE%\.diaries\responder.json"
 
 set "HIBERNATE_LOGLEVEL=OFF"
 set "LOGLEVEL=INFO"
+
+echo Generating responder build information...
+call gradlew.bat :diaries-responder:generateBuildInfo
+if errorlevel 1 (
+    set "EXIT_CODE=%ERRORLEVEL%"
+    echo ERROR: Unable to generate responder build information.
+    popd
+    endlocal & exit /b %EXIT_CODE%
+)
+
+rem Put the freshly generated resource first so an older copy under
+rem bin\main cannot take precedence.
+set "CLASSPATH=%GENERATED_BUILD_INFO_DIR%;%RESPONDER_DIR%\bin\main;%RESPONDER_DIR%\runtime\*"
 
 @echo on
 java -classpath "%CLASSPATH%" com.rsmaxwell.diaries.responder.Responder --config "%CONFIG_FILE%"
